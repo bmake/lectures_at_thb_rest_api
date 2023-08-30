053f55a7bea6b210895bfbdb4832df70fb4cdd33
Wenxin Hu <hu@th-brandenburg.de>
2023-08-30 16:34:34 +0200

    update vimeo API to use the new authentification method

--- routes/vimeo.js
+++ routes/vimeo.js
@@ -5,19 +5,21 @@ module.exports = app => {
   const sendResponse = require(path.join(__dirname, '../util/helperFunctions')).sendResponse
   app.get('/v1/vimeo/:videoID',
     (request, response) => {
-      const url = new URL(request.params.videoID, 'https://player.vimeo.com/video/').href+'/config';
+      const url = new URL(request.params.videoID, 'https://api.vimeo.com/videos/').href;
       console.log(url)
       axios.get(url, {
         headers: {
-          'Access-Control-Allow-Origin': '*'
+          'Authorization': 'Bearer 87dd0bc7206a05b9c716b853db535a47'
         }
       })
     .then(res => {
-      const streams = res.data.request.files.progressive;
+      console.log(res)
+      const streams = res.data.play.progressive;
+      console.log(streams)
       let links = streams.map(r => {
-        return {'quality': r.quality, 'url': r.url}
+        return {'quality': r.rendition, 'url': r.link}
       });
-      links = links.map(r => ({ ...r, thumbnail: res.data.video.thumbs.base }));
+      links = links.map(r => ({ ...r, thumbnail: res.data.pictures.base_link }));
       if (streams.length === 0) {
         sendResponse(response, 404, 'No video was found!', null)
       } else {
